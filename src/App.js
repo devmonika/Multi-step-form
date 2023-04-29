502ea69c651e4ee600523c6a192301f9a322001f
Monika <77955085+devmonika@users.noreply.github.com>
2023-04-29 23:58:16 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -37,7 +37,34 @@ function App() {
     console.log(formData);
   }
   return (
-    <div className="App bg-sky-900 text-white">
+    <div className="App bg-sky-900">
+        {/* nav */}
+
+        <div className="navbar bg-white">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+       
+      </ul>
+    </div>
+    <a className="btn btn-ghost normal-case text-xl">Logo</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Get started</a>
+  </div>
+</div>
+
+
      <h1 className='text-5xl text-white font-semibold pt-10'>Multi-step-form</h1>
         <div class="p-5">
             <form onSubmit={handleSubmit} class="mt-8 p-4">
